ea7f57c6c0f9404fc69b5907f5b8ba9141206f7a
jerinjv32 <jerinjvarghese32@gmail.com>
2025-12-14 14:33:52 +0530

    TextInput is included

--- app/collabCoding.jsx
+++ app/collabCoding.jsx
@@ -1,4 +1,4 @@
-import { Pressable, StyleSheet, Text, View } from 'react-native'
+import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Header from '../src/components/header'
 import Footer from '../src/components/footer'
@@ -13,8 +13,11 @@ const CollabCoding = () => {
           <Pressable >
             <Text style={[styles.text, styles.button]}>Generate Room</Text>
           </Pressable>
-          <View style={styles.lineBreak}></View>
-          <Text></Text>
+          <View style={styles.lineBreak}/>
+          <Text style={[styles.text, {textAlign: 'center'}]}>Enter the key shared by your friend</Text>
+          <View style={styles.keyInput}>
+            <TextInput maxLength={6} placeholder='Key' placeholderTextColor={'#999'} style={{textAlign: 'center'}}/>
+          </View>
         </ImageBackground>
         <Footer/>
     </SafeAreaView>
@@ -52,5 +55,13 @@ const styles = StyleSheet.create({
       height: 1,
       width: '90%',
       borderStyle: 'dashed',
+    },
+    keyInput:{
+      backgroundColor: '#212020',
+      borderColor: '#35315C',
+      borderWidth: 3,
+      borderRadius: 10,
+      width: 235,
+      height: 60,
     }
 })
