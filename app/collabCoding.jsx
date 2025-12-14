f448d9731028380f84a78bc4642841e0d5fbd5e4
jerinjv32 <jerinjvarghese32@gmail.com>
2025-12-14 17:41:45 +0530

    Collab coding design is completed

--- app/collabCoding.jsx
+++ app/collabCoding.jsx
@@ -1,4 +1,4 @@
-import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native'
+import { Pressable, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Header from '../src/components/header'
 import Footer from '../src/components/footer'
@@ -9,15 +9,38 @@ const CollabCoding = () => {
     <SafeAreaView style={{flex:1, backgroundColor: '#2F2F2F'}}>
         <Header/>
         <ImageBackground source={require('../assets/images/background.png')} resizeMode="cover" style={styles.background}>
-          <Text style={[styles.text, {marginTop : 50}]}>!{'\n\n'}Welcome to collab coding.{'\n'}Here you can work on the same{'\n'}code base with your friends.</Text>
+          {/* Welcome Text */}
+          <Text style={[styles.text, {marginTop : 10, marginBottom: 20}]}>!{'\n\n'}
+            Welcome to collab coding.{'\n'}Here you can work on the same{'\n'}code base with your friends.</Text>
+          {/* Welcoem text ends here */}
+
+          {/* Entering key */}
+          <Text style={[styles.text, {textAlign: 'center'}]}>Enter the key shared by{'\n'}your friend</Text>
+
+          <View style={styles.keyInput}>
+            <TextInput maxLength={6} placeholder='______' placeholderTextColor={'#999'} 
+            style={[styles.inputText,{textAlign: 'center'}]}/>
+          </View>
+          {/* Entering ends here */}
+
+          {/* Join Button */}
+          <TouchableOpacity>
+            <Text style={styles.joinBtn}>JOIN</Text>
+          </TouchableOpacity>
+          {/* Join button ends here */}
+
+          {/* Dashed line*/}
+          <View style={styles.lineBreak}/>
+          {/* Dashed line */}
+
+          {/* Generate room button */}
+          <Text style={[styles.text, {marginTop: 30, textAlign: 'center'}]}>Or, generate a room and{'\n'}share the generated key </Text>
           <Pressable >
             <Text style={[styles.text, styles.button]}>Generate Room</Text>
           </Pressable>
-          <View style={styles.lineBreak}/>
-          <Text style={[styles.text, {textAlign: 'center'}]}>Enter the key shared by your friend</Text>
-          <View style={styles.keyInput}>
-            <TextInput maxLength={6} placeholder='Key' placeholderTextColor={'#999'} style={{textAlign: 'center'}}/>
-          </View>
+          {/* Generate room button ends here */}
+
+
         </ImageBackground>
         <Footer/>
     </SafeAreaView>
@@ -63,5 +86,25 @@ const styles = StyleSheet.create({
       borderRadius: 10,
       width: 235,
       height: 60,
+      marginBottom: 20,
+      elevation: 5
+    },
+    inputText:{
+      fontSize: 20,
+      color: 'white',
+      letterSpacing: 10
+    },
+    joinBtn:{
+      color:'white', 
+      backgroundColor: '#35315C',
+      fontFamily: 'press-start-2p',
+      paddingTop: 20,
+      paddingBottom: 10,
+      paddingHorizontal: 20,
+      marginBottom: 40,
+      borderRadius: 10,
+      borderWidth: 3,
+      borderColor: '#35315C',
+      elevation: 5,
     }
 })
