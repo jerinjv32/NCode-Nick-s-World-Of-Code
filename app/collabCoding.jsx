14e2c8b5e32fde58861a7d0b6318fa4b6745ca63
jerinjv32 <jerinjvarghese32@gmail.com>
2025-12-14 15:22:50 +0530

    The key input is below but footer is going up

--- app/collabCoding.jsx
+++ app/collabCoding.jsx
@@ -1,25 +1,32 @@
-import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native'
+import { KeyboardAvoidingView, Platform, Pressable, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Header from '../src/components/header'
 import Footer from '../src/components/footer'
-import { ImageBackground } from 'react-native'
 
 const CollabCoding = () => {
   return (
-    <SafeAreaView style={{flex:1, backgroundColor: '#2F2F2F'}}>
+    <SafeAreaView style={{flex:1, backgroundColor: '#2F2F2F',}}>
         <Header/>
-        <ImageBackground source={require('../assets/images/background.png')} resizeMode="cover" style={styles.background}>
-          <Text style={[styles.text, {marginTop : 50}]}>!{'\n\n'}Welcome to collab coding.{'\n'}Here you can work on the same{'\n'}code base with your friends.</Text>
-          <Pressable >
-            <Text style={[styles.text, styles.button]}>Generate Room</Text>
-          </Pressable>
-          <View style={styles.lineBreak}/>
-          <Text style={[styles.text, {textAlign: 'center'}]}>Enter the key shared by your friend</Text>
-          <View style={styles.keyInput}>
-            <TextInput maxLength={6} placeholder='Key' placeholderTextColor={'#999'} style={{textAlign: 'center'}}/>
-          </View>
-        </ImageBackground>
-        <Footer/>
+          <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={{flex: 1}}
+          keyboardVerticalOffset={Platform.OS === 'ios' ? 100 : 0}>
+          <ScrollView contentContainerStyle={{flexGrow: 1,alignItems: 'center', justifyContent: 'flex-start'}}
+            keyboardShouldPersistTaps="handled">
+            <Text style={[styles.text, {marginTop : 50}]}>!{'\n\n'}Welcome to collab coding.{'\n'}
+              Here you can work on the same{'\n'}code base with your friends.</Text>
+            <Pressable >
+              <Text style={[styles.text, styles.button]}>Generate Room</Text>
+            </Pressable>
+            <View style={styles.lineBreak}/>
+            <Text style={[styles.text, {textAlign: 'center'}]}>Enter the key shared by your friend</Text>
+            <View style={styles.keyInput}>
+              <TextInput maxLength={6} placeholder='Key' placeholderTextColor={'#999'} 
+              style={[styles.inputText ,{textAlign: 'center'}]}/>
+            </View>
+          </ScrollView>
+        </KeyboardAvoidingView>
+      <View style={{ position: 'absolute', bottom: 48, left: 0, right: 0 }}>
+        <Footer />
+      </View>
     </SafeAreaView>
   )
 }
@@ -27,11 +34,6 @@ const CollabCoding = () => {
 export default CollabCoding
 
 const styles = StyleSheet.create({
-    background:{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-    },
     text:{
       color: 'white',
       fontSize: 12,
@@ -63,5 +65,8 @@ const styles = StyleSheet.create({
       borderRadius: 10,
       width: 235,
       height: 60,
+    },
+    inputText:{
+      fontSize: 20,
     }
 })
