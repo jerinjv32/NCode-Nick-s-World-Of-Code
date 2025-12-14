887a90530d57047026aff9c91b5cf52b1674630e
jerinjv32 <jerinjvarghese32@gmail.com>
2025-12-14 11:43:57 +0530

    Collab coding design started

--- app/collabCoding.jsx
+++ app/collabCoding.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native'
+import { Pressable, StyleSheet, Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Header from '../src/components/header'
 import Footer from '../src/components/footer'
@@ -9,6 +9,12 @@ const CollabCoding = () => {
     <SafeAreaView style={{flex:1, backgroundColor: '#2F2F2F'}}>
         <Header/>
         <ImageBackground source={require('../assets/images/background.png')} resizeMode="cover" style={styles.background}>
+          <Text style={[styles.text, {marginTop : 50}]}>!{'\n\n'}Welcome to collab coding.{'\n'}Here you can work on the same{'\n'}code base with your friends.</Text>
+          <Pressable >
+            <Text style={[styles.text, styles.button]}>Generate Room</Text>
+          </Pressable>
+          <View style={styles.lineBreak}></View>
+          <Text></Text>
         </ImageBackground>
         <Footer/>
     </SafeAreaView>
@@ -21,6 +27,30 @@ const styles = StyleSheet.create({
     background:{
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'flex-start',
     },
+    text:{
+      color: 'white',
+      fontSize: 12,
+      fontFamily: 'press-start-2p',
+      padding: 10,
+      lineHeight: 20
+    },
+    button:{
+      backgroundColor: '#212020',
+      borderColor: '#35315C',
+      borderWidth: 3,
+      borderRadius: 10,
+      paddingHorizontal: 50,
+      paddingVertical: 20,
+      elevation: 5,
+      margin: 40 
+    },
+    lineBreak:{
+      borderColor: 'white',
+      borderWidth: 3,
+      height: 1,
+      width: '90%',
+      borderStyle: 'dashed',
+    }
 })
