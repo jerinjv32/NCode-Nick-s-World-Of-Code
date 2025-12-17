a7dc00f0de32c3812ef6b072dcf20a07c84c9641
JOyAL229 <joyaljosephcu@gmail.com>
2025-12-17 17:16:06 +0530

    Login page is designed

--- app/index.jsx
+++ app/index.jsx
@@ -1,16 +1,41 @@
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, Touchable, TouchableOpacity } from 'react-native'
 import { router } from 'expo-router'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { ImageBackground } from 'react-native'
 
-const Title = () => {  
+const Title = () => {
   return (
     <SafeAreaView style={styles.container}>
-        <ImageBackground source={require('../assets/images/background.png')} resizeMode='cover'>
-            <View style={[styles.button, styles.shadow]}>
-              <Text style={styles.title} onPress={()=>{router.replace('/home')}}><Text style={{color: '#6760AB'}}>N</Text>CODE</Text>
-          </View>
-        </ImageBackground>
+      <View>
+        {/* logo */}
+        <View style={[styles.button, styles.shadow]}>
+          <Text style={styles.title}><Text style={{ color: '#6760AB', }}>N</Text>CODE</Text>
+        </View>
+        {/* username */}
+        <View style={styles.inputBox} >
+          <Text style={styles.text}>
+            username
+          </Text>
+        </View>
+        {/* password */}
+        <View style={styles.inputBox}>
+          <Text style={styles.text}>
+            password
+          </Text>
+        </View>
+        <Text style={[styles.text, { textAlign: 'right' }]}>
+          Forgot Password?
+        </Text>
+        {/* sign in button */}
+        <TouchableOpacity onPress={() => router.push('/home')} >
+          <Text style={styles.signIntext}>
+            Sign In
+          </Text>
+        </TouchableOpacity>
+        <Text style={[styles.text, { marginTop: 20, textAlign: 'center' }]}>
+          New? Sign Up
+        </Text>
+      </View>
     </SafeAreaView>
   )
 }
@@ -18,27 +43,58 @@ const Title = () => {
 export default Title
 
 const styles = StyleSheet.create({
-    container:{
-        backgroundColor: '#2F2F2F',
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    title:{
-        fontFamily: 'press-start-2p',
-        fontSize: 60,
-        color: 'white',
-    },
-    button:{
-      borderWidth: 3,
-      borderColor: '#35315C',
-      paddingTop: 30,
-      paddingHorizontal: 20,
-      borderRadius: 10,
-      backgroundColor: '#212020',
-    },
-    shadow:{
-      shadowColor: '#171717',
-      elevation: 5
-    }
+  container: {
+    backgroundColor: '#2F2F2F',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  title: {
+    fontFamily: 'press-start-2p',
+    fontSize: 60,
+    color: 'white',
+  },
+  button: {
+    borderWidth: 3,
+    borderColor: '#35315C',
+    paddingTop: 30,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    backgroundColor: '#212020',
+    marginBottom: 80,
+  },
+  shadow: {
+    shadowColor: '#171717',
+    elevation: 5
+  },
+  inputBox: {
+    backgroundColor: '#212020',
+    height: 75,
+    borderRadius: 10,
+    marginVertical: 10,
+    elevation: 5,
+    shadowColor: '#171717',
+    justifyContent: 'flex-start',
+  },
+  text: {
+    color: '#999',
+    fontFamily: 'press-start-2p',
+    fontSize: 9,
+    padding: 10,
+  },
+  signIntext: {
+    borderWidth: 3,
+    borderColor: '#35315C',
+    backgroundColor: '#35315C',
+    elevation: 10,
+    shadowColor: '#171717',
+    color: 'white',
+    fontFamily: 'press-start-2p',
+    fontSize: 9,
+    textAlign: 'center',
+    paddingTop: 15,
+    paddingBottom: 10,
+    marginTop: 20,
+    borderRadius: 10,
+  }
 })
