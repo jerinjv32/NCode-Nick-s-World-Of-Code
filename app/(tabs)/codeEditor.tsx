f4693cead10e783502b4a170aff5b8a282861e39
jerinjv32 <jerinjvarghese32@gmail.com>
2026-01-18 18:31:15 +0530

    web view is added

--- app/(tabs)/codeEditor.tsx
+++ app/(tabs)/codeEditor.tsx
@@ -2,8 +2,9 @@ import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { Dropdown } from 'react-native-element-dropdown'
 import { useState } from 'react'
-import CodeEditor, { CodeEditorSyntaxStyles } from '@rivascva/react-native-code-editor';
 import { commonFontColor, darkGrey, grey, lightPurple, purple } from '../../src/styles/colors'
+import { codeEditorHtml } from '../../src/components/monacoCodeEditor'
+import { WebView } from 'react-native-webview';
 
 const lang = [
     {
@@ -19,64 +20,57 @@ const lang = [
         value: '3'
     }
 ]
+
 const codeEditor = () => {
     const [value, setValue] = useState(null);
     return (
-        <SafeAreaView edges={['bottom']}>
+        <SafeAreaView edges={['bottom']} style={{ flex: 1, backgroundColor: 'white' }}>
             <View style={styles.tab}>
-                <Dropdown 
-                data={lang}
-                style={{width: 170, 
-                    backgroundColor: darkGrey, 
-                    paddingHorizontal: 10,
-                    paddingVertical: 5,
-                    borderWidth: 3,
-                    borderColor: purple,
-                    borderRadius: 10,
-                    elevation: 5
-                }}
-                containerStyle={{
-                    backgroundColor: purple,
-                    borderWidth: 3,
-                    borderColor: purple,
-                    borderRadius: 10,
-                    elevation: 5
-                }}
-                itemTextStyle={{
-                    color: commonFontColor,
-                    fontSize: 10,
-                }}
-                selectedTextStyle={{
-                    fontSize: 10,
-                    color: commonFontColor,
-                }}
-                activeColor='#2c2848ff'
-                placeholderStyle={{
-                    fontSize: 10, 
-                    color: commonFontColor
-                }}
-                valueField={'value'}
-                labelField={'label'}                
-                fontFamily='press-start-2p'
-                value={value}
-                onChange={item => {
-                    setValue(item.value);
-                }}
+                <Dropdown
+                    data={lang}
+                    style={{
+                        width: 170,
+                        backgroundColor: darkGrey,
+                        paddingHorizontal: 10,
+                        paddingVertical: 5,
+                        borderWidth: 3,
+                        borderColor: purple,
+                        borderRadius: 10,
+                        elevation: 5
+                    }}
+                    containerStyle={{
+                        backgroundColor: purple,
+                        borderWidth: 3,
+                        borderColor: purple,
+                        borderRadius: 10,
+                        elevation: 5
+                    }}
+                    itemTextStyle={{
+                        color: commonFontColor,
+                        fontSize: 10,
+                    }}
+                    selectedTextStyle={{
+                        fontSize: 10,
+                        color: commonFontColor,
+                    }}
+                    activeColor='#2c2848ff'
+                    placeholderStyle={{
+                        fontSize: 10,
+                        color: commonFontColor
+                    }}
+                    valueField={'value'}
+                    labelField={'label'}
+                    fontFamily='press-start-2p'
+                    value={value}
+                    onChange={item => {
+                        setValue(item.value);
+                    }}
                 >
                 </Dropdown>
                 <TouchableOpacity activeOpacity={0.5}>
                     <Text style={styles.runBtn}>RUN</Text>
                 </TouchableOpacity>
             </View>
-            <CodeEditor
-                style={{
-                    inputLineHeight: 26,
-                    highlighterLineHeight: 26,
-                }}
-                language="python"
-                syntaxStyle={CodeEditorSyntaxStyles.dracula}
-                showLineNumbers
-            />
         </SafeAreaView>
     )
 }
@@ -84,7 +78,7 @@ const codeEditor = () => {
 export default codeEditor
 
 const styles = StyleSheet.create({
-    runBtn:{
+    runBtn: {
         color: commonFontColor,
         backgroundColor: lightPurple,
         fontSize: 10,
@@ -98,7 +92,7 @@ const styles = StyleSheet.create({
         marginLeft: 10,
         marginRight: 10,
     },
-    tab:{
+    tab: {
         backgroundColor: grey,
         flexDirection: 'row',
         justifyContent: 'flex-end',
---
+++ src/components/monacoCodeEditor.ts
@@ -0,0 +1,6 @@
+export const codeEditorHtml = `
+<meta name="viewport" content="width=device-width">
+<body style="background-color: grey">
+    <h1>Hello</h1>
+</body>
+`;
